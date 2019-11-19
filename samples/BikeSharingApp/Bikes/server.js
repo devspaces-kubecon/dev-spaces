b6e575faa4f3020de237f92b8f83dba352b4e7d3
devspaces-kubecon <57688084+devspaces-kubecon@users.noreply.github.com>
2019-11-19 15:15:50 -0800

    Update server.js

--- samples/BikeSharingApp/Bikes/server.js
+++ samples/BikeSharingApp/Bikes/server.js
@@ -229,6 +229,7 @@ app.get('/api/bikes/:bikeId', function(req, res) {
         }
 
         var theBike = result;
+        //my change PK
         // Hard code image url *FIX ME*
         //theBike.imageUrl = "/static/logo.svg";
         theBike.id = theBike._id;
